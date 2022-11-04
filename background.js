675bd89a107f83831abd07e91605ff03bc3cabc6
Vinz <intensity67@gmail.com>
2022-11-04 14:47:50 +0800

    added api key, fixing issue uncaught promise

--- background.js
+++ background.js
@@ -6,10 +6,10 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
 
     if(msg.name == "fetchWords"){
 
-        const apiKey = "";
+        const apiKey = "w9kjsupvpof48df9ri74twu55f8jtke5yy2qqqrdwxlq9jeog";
         const currentDate = new Date().toISOString().slice(0, 10); //2020-01-01
         const apiCall = "https://api.wordnik.com/v4/words.json/wordOfTheDay?date="+currentDate+"&api_key=" + apiKey;
-    
+
         // We call api
         // wait for response
         // send response
@@ -21,6 +21,12 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
         const wordsDescObj = [" kind of Japanese woodblock print, privately commissioned for special occasions such as the New Year"
 
         ];
+
+        console.log(apiCall);
+
+        fetch("https://api.wordnik.com/v4/words.json/wordOfTheDay?date=2020-01-01&api_key=w9kjsupvpof48df9ri74twu55f8jtke5yy2qqqrdwxlq9jeog")
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
         var number = 0;
 
