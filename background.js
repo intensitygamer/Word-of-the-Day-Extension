f41b415b91d37fb927a05e493f2dac06a4faa987
Vinz <intensity67@gmail.com>
2022-11-07 20:44:03 +0800

    fix - API calling from Wordnick by changing caller in the front end script instead from background

--- background.js
+++ background.js
@@ -2,31 +2,19 @@
 
 //Listen for messages
 
+async function fetchWord(apiCall) {
+    const response = await fetch(apiCall);
+    // waits until the request completes...
+    console.log("from async" + response);
+  }
+
+
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
 
     if(msg.name == "fetchWords"){
 
-        // const apiKey = "w9kjsupvpof48df9ri74twu55f8jtke5yy2qqqrdwxlq9jeog";
-        // const currentDate = new Date().toISOString().slice(0, 10); //2020-01-01
-        // const apiCall = "https://api.wordnik.com/v4/words.json/wordOfTheDay?date="+currentDate+"&api_key=" + apiKey;
-
-        //https://api.wordnik.com/v4/words.json/wordOfTheDay?date=2020-01-01&api_key=w9kjsupvpof48df9ri74twu55f8jtke5yy2qqqrdwxlq9jeog
-
-
-        // We call api
-
-        var resdata;
-
-        fetchwordres = fetch(apiCall)
-        .then((response)  =>  response.json() )
-        .then((data) => { console.log(data)});
-
-
-        response(fetchwordres);
-
-        // Send response
-        //response({word: wordsObj[number], desc: wordsDescObj[number]});
-
     }
 
 })
+
+
--- script.js
+++ script.js
@@ -2,23 +2,33 @@
 
 chrome.runtime.sendMessage({name: "fetchWords"}, (response) => {
 
+    console.log("response from background- ");
+    console.log(response);
+
     const apiKey = "w9kjsupvpof48df9ri74twu55f8jtke5yy2qqqrdwxlq9jeog";
     const currentDate = new Date().toISOString().slice(0, 10); //2020-01-01
 
     const apiCall = "https://api.wordnik.com/v4/words.json/wordOfTheDay?date="+currentDate+"&api_key=" + apiKey;
 
-    var res;
-
-    fetch(apiCall)
+    const res =  fetch(apiCall)
     .then((response) => response.json())
-    .then((data) => {  res = json_encode(data); } );
+    .then((data) => {
 
-    // Wait Response
+        // Wait Response
 
-    console.log(res);
-    //Update Display on Content Script
+        console.log(data);
 
-    document.querySelector('h1').innerHTML  = res.name;
-    document.querySelector('p').innerHTML   = response.definitions[0];
+        wordObj = data;
+
+        //Update Display on Content Script
+
+        document.querySelector('h1').innerHTML  = wordObj.word;
+        document.querySelector('p').innerHTML   = wordObj.note;
+
+    });
+
+
+
+
 
 });
