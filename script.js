bf30770a20a3a2fed1280d8e82d8a4f0f51cec63
Vinz <intensity67@gmail.com>
2022-11-17 19:21:36 +0800

    update word display defeinitions from its list rather than notes

--- script.js
+++ script.js
@@ -14,16 +14,27 @@ chrome.runtime.sendMessage({name: "fetchWords"}, (response) => {
     .then((response) => response.json())
     .then((data) => {
 
-        // Wait Response
 
-        console.log(data);
+        let minimum, maximum, no_defs;
 
         wordObj = data;
+
+        console.log(wordObj);
+
+        minimum = 0;
+
+        maximum = parseInt(wordObj.definitions.length)- 1;
+
+        no_defs = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+
+        console.log(no_defs);
 
         //Update Display on Content Script
 
         document.querySelector('h1').innerHTML  = wordObj.word;
-        document.querySelector('p').innerHTML   = wordObj.note;
+
+        document.querySelector('p').innerHTML   = wordObj.definitions[no_defs].text;
+
 
     });
 
